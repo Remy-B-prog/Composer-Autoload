56c2035472a0b996bbc8c163cefbb4556d05a7c2
BERNARDIN <remy.bernardin@cgi.com>
2023-03-03 11:34:12 +0100

    CowSay Questes

--- public/index.php
+++ public/index.php
@@ -4,6 +4,13 @@ require '../vendor/autoload.php';
 
 
 use App\Hello;
+use CowSay\Cow;
 
-$hello = new Hello;
-$hello->talk();
+$bessie = new Cow('Hello, Farm!');
+
+// store the output in a variable
+$output = $bessie->say();
+echo $output;
+
+// or just echo the object for direct output
+echo $bessie;
--- src/Hello.php
+++ src/Hello.php
@@ -3,7 +3,33 @@
 namespace App;
 
 class Hello {
-    public function talk() : void {
-        echo 'Hello World';
+    public string $name;
+    public string $firstname;
+    public string $adresse;
+    public $dateOfBird;
+    
+    public function __construct(string $name, string $firstname, string $adresse, $dateOfBird){
+        $this->setDateOfBird($dateOfBird);
+        $this->name = $name;
+        $this->firstname = $firstname;
+        $this->adresse = $adresse;
+    }
+
+    public function getPersoninfo(){
+        echo "$this->name $this->firstname $this->adresse " . $this->dateOfBird->format('d/m/Y');
+    }
+ 
+    public function setDateOfBird($dateOfBird) {
+        $this->dateOfBird = \DateTime::createFromFormat('d/m/Y', $dateOfBird);
+    }
+
+    public function getDateOfBird() {
+       return $this->dateOfBird;
+    }
+    
+    public function getAge() {
+        $today = new \DateTime();
+        $diff = $today->diff($this->getDateOfBird());
+        echo " $diff->y ans";
     }
 }
